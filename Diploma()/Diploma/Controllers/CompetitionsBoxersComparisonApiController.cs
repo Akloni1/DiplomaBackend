3e81a8d5aed10f07dfc95b694f43287657004187
Akloni1 <61523151+Akloni1@users.noreply.github.com>
2022-03-20 06:18:42 +0300

    add function Comparison(add CompetitionsBoxersComparisonApiController, BoxersViewModel )

--- Diploma()/Diploma/Controllers/CompetitionsBoxersComparisonApiController.cs
+++ Diploma()/Diploma/Controllers/CompetitionsBoxersComparisonApiController.cs
@@ -4,6 +4,7 @@ using Diploma.ViewModels.Boxers;
 using Diploma.ViewModels.CompetitionsBoxers;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -24,7 +25,7 @@ namespace Diploma.Controllers
         [HttpGet("{id}")]  // GET: /api/competitions/boxer/1
         [ProducesResponseType(200, Type = typeof(IEnumerable<BoxerViewModel>))]
         [ProducesResponseType(404)]
-        public ActionResult<IEnumerable<BoxerViewModel>> GetBoxersByIdCompetition(int id)
+        public ActionResult<IEnumerable<BoxersViewModel>> GetBoxersByIdCompetition(int id)
         {
             var competitionsBoxers = _mapper.Map<IEnumerable<CompetitionsBoxers>, IEnumerable<CompetitionsBoxersViewModel>>(_context.CompetitionsBoxers.Where(a => a.CompetitionsId == id).ToList());
             var idBoxers = competitionsBoxers.Select(h => h.BoxerId).ToList();
@@ -43,52 +44,107 @@ namespace Diploma.Controllers
                 {
                     boxersConcat = (boxersConcat ?? Enumerable.Empty<BoxerViewModel>()).Concat(boxers[i + 1] ?? Enumerable.Empty<BoxerViewModel>());
                 }
+
+
             }
             else return NotFound();
 
 
-
             DateTime timeNow = DateTime.Now;
-            List<BoxerViewModel> list_14_16;
-            List<BoxerViewModel> list_16_18;
-            List<BoxerViewModel> list_18_25;
-            List<BoxerViewModel> list_25_30;
-            List<BoxerViewModel> list_30_40;
+            // var res = new { };
 
-            foreach (var boxer in boxersConcat)
+            List<int> idDistributed = new List<int>();
+
+            // IEnumerable<BoxersViewModel> res ;
+            List<BoxersViewModel> res = new List<BoxersViewModel>();
+            // BoxersViewModel couple = new BoxersViewModel();
+
+            foreach (var boxer1 in boxersConcat)
             {
-                var dateOfBirth = (DateTime)boxersConcat.Select(a => a.DateOfBirth).First();
-                int age = timeNow.Year - dateOfBirth.Year;
-               
-                if(age>=14 && age < 16)
-                {
-                   
-                }
-                else if (age >= 16 && age < 18)
-                {
 
-                }
-                else if (age >= 18 && age < 25)
+                if (idDistributed.Contains(boxer1.BoxerId))
                 {
+                    continue;
+                }
 
-                }
-                else if (age >= 25 && age < 31)
-                {
 
-                }
-                else if (age >= 31 && age < 41)
-                {
 
-                }
-                else
+
+
+                var dateOfBirth1 = (DateTime)boxer1.DateOfBirth;
+                double age1 = timeNow.Year - dateOfBirth1.Year;
+                foreach (var boxer2 in boxersConcat)
                 {
-                   
+                    if (idDistributed.Contains(boxer2.BoxerId))
+                    {
+                        continue;
+                    }
+                    if (idDistributed.Contains(boxer1.BoxerId))
+                    {
+                        continue;
+                    }
+
+                    var dateOfBirth2 = (DateTime)boxer2.DateOfBirth;
+                    double age2 = timeNow.Year - dateOfBirth2.Year;
+                    if (boxer1.BoxerId == boxer2.BoxerId) continue;
+
+                    if (age1 / 100d * 20d >= Math.Abs(age1 - age2))
+                    {
+                        if ((double)boxer1.Weight / 100d * 20d >= Math.Abs((int)boxer1.Weight - (int)boxer2.Weight))
+                        {
+                            if ((double)boxer1.NumberOfFightsHeld / 100d * 20d >= Math.Abs((int)boxer1.NumberOfFightsHeld - (int)boxer2.NumberOfFightsHeld))
+                            {
+                                if ((double)boxer1.NumberOfWins / 100d * 20d >= Math.Abs((int)boxer1.NumberOfWins - (int)boxer2.NumberOfWins))
+                                {
+                                    if ((double)boxer1.TrainingExperience / 100d * 20d >= Math.Abs((double)boxer1.TrainingExperience - (double)boxer2.TrainingExperience))
+                                    {
+                                        if (((string)boxer1.Discharge).Equals((string)boxer2.Discharge))
+                                        {
+                                            BoxersViewModel couple = new BoxersViewModel();
+                                            couple.boxer1 = boxer1;
+                                            couple.boxer2 = boxer2;
+                                            res.Add(couple);
+
+
+
+                                            //  var  couple = new { boxer1, boxer2 };
+                                            //нужно реализовать добавление couple в обьект где будут все пары 
+
+                                            idDistributed.Add(boxer1.BoxerId);
+                                            idDistributed.Add(boxer2.BoxerId);
+
+
+                                            //  res = { };
+                                            //   jj = (jj ?? Enumerable.Empty <dynamic>()).Concat(couple ?? Enumerable.Empty<dynamic>());
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
+
+
             }
-           /* var dateOfBirth = (DateTime)boxersConcat.Select(a => a.DateOfBirth).First();
-            int age = timeNow.Year - dateOfBirth.Year;*/
 
-            return Ok(boxersConcat);
+            List<BoxerViewModel> notPaired = boxersConcat.ToList();
+            foreach (int idDist in idDistributed)
+            {
+                var model = (notPaired.Where(a => a.BoxerId == idDist).First());
+                notPaired.Remove(model);
+
+            }
+
+            var final = new
+            {
+                res,
+                notPaired = new
+                {
+                    notPaired
+                }
+            };
+
+            return Ok(final);
         }
 
     }
---
+++ Diploma()/Diploma/ViewModels/Boxers/BoxersViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Diploma.ViewModels.Boxers
+{
+    public class BoxersViewModel
+    {
+        public BoxerViewModel boxer1 { get; set; }
+        public BoxerViewModel boxer2 { get; set; }
+    }
+}
